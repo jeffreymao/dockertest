2c1b895e32dc1bdb85726caa870a005dfca6b606
毛家飞 <jiafei.mao@fir.ai>
2018-11-28 14:19:42 +0800

    add project dockertest

---
+++ src/main/java/com/coffee/dockertest/DockerApplication.java
@@ -0,0 +1,17 @@
+package com.coffee.dockertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class DockerApplication  
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(DockerApplication.class, args);
+    }
+}
